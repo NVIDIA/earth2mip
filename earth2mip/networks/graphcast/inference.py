443cb07b5bcc9508cc7e38d06a07972fe271d63c
Noah D. Brenowitz <nbrenowitz@nvidia.com>
2023-08-18 18:11:16 -0700

    vendor the rollout.py

--- earth2mip/networks/graphcast/inference.py
+++ earth2mip/networks/graphcast/inference.py
@@ -14,7 +14,7 @@ from graphcast import checkpoint
 from graphcast import data_utils
 from graphcast import graphcast
 from graphcast import normalization
-from graphcast import rollout
+from earth2mip.networks.graphcast import rollout
 from graphcast import xarray_jax
 from graphcast import xarray_tree
 from IPython.display import HTML
@@ -157,33 +157,216 @@ def data_valid_for_model(
     )
 
 
+class Graphcast:
+    def __init__(self, root):
+        model_name = "GraphCast - ERA5 1979-2017 - resolution 0.25 - pressure levels 37 - mesh 2to6 - precipitation input and output.npz"
+        checkpoint_path = os.path.join(root, "params", model_name)
+        # load checkpoint:
+        with open(checkpoint_path, "rb") as f:
+            ckpt = checkpoint.load(f, graphcast.CheckPoint)
+            params = ckpt.params
+            state = {}
+
+        model_config = ckpt.model_config
+        task_config = ckpt.task_config
+        self.task_config = task_config
+        print("Model description:\n", ckpt.description, "\n")
+        print("Model license:\n", ckpt.license, "\n")
+
+        # load stats
+        with open(os.path.join(root, "stats/diffs_stddev_by_level.nc"), "rb") as f:
+            diffs_stddev_by_level = xarray.load_dataset(f).compute()
+        with open(os.path.join(root, "stats/mean_by_level.nc"), "rb") as f:
+            mean_by_level = xarray.load_dataset(f).compute()
+        with open(os.path.join(root, "stats/stddev_by_level.nc"), "rb") as f:
+            stddev_by_level = xarray.load_dataset(f).compute()
+
+        # jit the stuff
+        # @title Build jitted functions, and possibly initialize random weights
+        def construct_wrapped_graphcast(
+            model_config: graphcast.ModelConfig, task_config: graphcast.TaskConfig
+        ):
+            """Constructs and wraps the GraphCast Predictor."""
+            # Deeper one-step predictor.
+            predictor = graphcast.GraphCast(model_config, task_config)
+
+            # Modify inputs/outputs to `graphcast.GraphCast` to handle conversion to
+            # from/to float32 to/from BFloat16
+            predictor = casting.Bfloat16Cast(predictor)
+
+            # Modify inputs/outputs to `casting.Bfloat16Cast` so the casting to/from
+            # BFloat16 happens after applying normalization to the inputs/targets.
+            predictor = normalization.InputsAndResiduals(
+                predictor,
+                diffs_stddev_by_level=diffs_stddev_by_level,
+                mean_by_level=mean_by_level,
+                stddev_by_level=stddev_by_level,
+            )
+
+            # # Wraps everything so the one-step model can produce trajectories.
+            # predictor = autoregressive.Predictor(predictor, gradient_checkpointing=True)
+            return predictor
+
+        @hk.transform_with_state
+        def run_forward(model_config, task_config, inputs, targets_template, forcings):
+            predictor = construct_wrapped_graphcast(model_config, task_config)
+            return predictor(
+                inputs, targets_template=targets_template, forcings=forcings
+            )
+
+        # Jax doesn't seem to like passing configs as args through the jit. Passing it
+        # in via partial (instead of capture by closure) forces jax to invalidate the
+        # jit cache if you change configs.
+        def with_configs(fn):
+            return functools.partial(
+                fn, model_config=model_config, task_config=task_config
+            )
+
+        # Always pass params and state, so the usage below are simpler
+        def with_params(fn):
+            return functools.partial(fn, params=params, state=state)
+
+        # Our models aren't stateful, so the state is always empty, so just return the
+        # predictions. This is requiredy by our rollout code, and generally simpler.
+        def drop_state(fn):
+            return lambda **kw: fn(**kw)[0]
+
+        init_jitted = jax.jit(with_configs(run_forward.init))
+
+        if params is None:
+            params, state = init_jitted(
+                rng=jax.random.PRNGKey(0),
+                inputs=eval_inputs,
+                targets_template=eval_targets,
+                forcings=eval_forcings,
+            )
+
+        self.run_forward_jitted = drop_state(
+            with_params(jax.jit(with_configs(run_forward.apply)))
+        )
+
+    def __call__(self, eval_inputs, eval_targets, eval_forcings):
+        """
+
+        inputs::
+
+            xarray.Dataset {
+        dimensions:
+                batch = 1 ;
+                time = 2 ;
+                lat = 721 ;
+                lon = 1440 ;
+                level = 37 ;
+
+        variables:
+                float32 2m_temperature(batch, time, lat, lon) ;
+                float32 mean_sea_level_pressure(batch, time, lat, lon) ;
+                float32 10m_v_component_of_wind(batch, time, lat, lon) ;
+                float32 10m_u_component_of_wind(batch, time, lat, lon) ;
+                float32 total_precipitation_6hr(batch, time, lat, lon) ;
+                float32 temperature(batch, time, level, lat, lon) ;
+                float32 geopotential(batch, time, level, lat, lon) ;
+                float32 u_component_of_wind(batch, time, level, lat, lon) ;
+                float32 v_component_of_wind(batch, time, level, lat, lon) ;
+                float32 vertical_velocity(batch, time, level, lat, lon) ;
+                float32 specific_humidity(batch, time, level, lat, lon) ;
+                float32 toa_incident_solar_radiation(batch, time, lat, lon) ;
+                float32 year_progress_sin(batch, time) ;
+                float32 year_progress_cos(batch, time) ;
+                float32 day_progress_sin(batch, time, lon) ;
+                float32 day_progress_cos(batch, time, lon) ;
+                float32 geopotential_at_surface(lat, lon) ;
+                float32 land_sea_mask(lat, lon) ;
+                float32 lon(lon) ;
+                        lon:long_name = longitude ;
+                        lon:units = degrees_east ;
+                float32 lat(lat) ;
+                        lat:long_name = latitude ;
+                        lat:units = degrees_north ;
+                int32 level(level) ;
+                timedelta64[ns] time(time) ;
+
+            // global attributes:
+            }
+
+        targets::
+
+            xarray.Dataset {
+            dimensions:
+                    batch = 1 ;
+                    time = 12 ;
+                    lat = 721 ;
+                    lon = 1440 ;
+                    level = 37 ;
+
+            variables:
+                    float32 2m_temperature(batch, time, lat, lon) ;
+                    float32 mean_sea_level_pressure(batch, time, lat, lon) ;
+                    float32 10m_v_component_of_wind(batch, time, lat, lon) ;
+                    float32 10m_u_component_of_wind(batch, time, lat, lon) ;
+                    float32 total_precipitation_6hr(batch, time, lat, lon) ;
+                    float32 temperature(batch, time, level, lat, lon) ;
+                    float32 geopotential(batch, time, level, lat, lon) ;
+                    float32 u_component_of_wind(batch, time, level, lat, lon) ;
+                    float32 v_component_of_wind(batch, time, level, lat, lon) ;
+                    float32 vertical_velocity(batch, time, level, lat, lon) ;
+                    float32 specific_humidity(batch, time, level, lat, lon) ;
+                    float32 lon(lon) ;
+                            lon:long_name = longitude ;
+                            lon:units = degrees_east ;
+                    float32 lat(lat) ;
+                            lat:long_name = latitude ;
+                            lat:units = degrees_north ;
+                    int32 level(level) ;
+                    timedelta64[ns] time(time) ;
+
+            // global attributes:
+            }
+
+        forcings::
+
+            xarray.Dataset {
+            dimensions:
+                    batch = 1 ;
+                    time = 12 ;
+                    lat = 721 ;
+                    lon = 1440 ;
+
+            variables:
+                    float32 toa_incident_solar_radiation(batch, time, lat, lon) ;
+                    float32 year_progress_sin(batch, time) ;
+                    float32 year_progress_cos(batch, time) ;
+                    float32 day_progress_sin(batch, time, lon) ;
+                    float32 day_progress_cos(batch, time, lon) ;
+                    float32 lon(lon) ;
+                            lon:long_name = longitude ;
+                            lon:units = degrees_east ;
+                    float32 lat(lat) ;
+                            lat:long_name = latitude ;
+                            lat:units = degrees_north ;
+                    timedelta64[ns] time(time) ;
+
+            // global attributes:
+            }
+
+        """
+        return rollout.chunked_prediction(
+            self.run_forward_jitted,
+            rng=jax.random.PRNGKey(0),
+            inputs=eval_inputs,
+            targets_template=eval_targets * np.nan,
+            forcings=eval_forcings,
+        )
+
+
 def main():
-    model_name = "GraphCast - ERA5 1979-2017 - resolution 0.25 - pressure levels 37 - mesh 2to6 - precipitation input and output.npz"
     root = "/lustre/fsw/sw_earth2_ml/graphcast/"
-    checkpoint_path = os.path.join(root, "params", model_name)
-
-    # load checkpoint:
-    with open(checkpoint_path, "rb") as f:
-        ckpt = checkpoint.load(f, graphcast.CheckPoint)
-        params = ckpt.params
-        state = {}
-
-    model_config = ckpt.model_config
-    task_config = ckpt.task_config
-    print("Model description:\n", ckpt.description, "\n")
-    print("Model license:\n", ckpt.license, "\n")
+    model = Graphcast(root)
 
     # load dataset
     dataset_filename = os.path.join(
         root, "dataset", "source-era5_date-2022-01-01_res-0.25_levels-37_steps-12.nc"
     )
-    if not data_valid_for_model(
-        os.path.basename(dataset_filename), model_config, task_config
-    ):
-        raise ValueError(
-            f"Invalid dataset file {os.path.basename(dataset_filename)}, rerun the cell above and choose a valid dataset file."
-        )
-
     with open(dataset_filename, "rb") as f:
         example_batch = xarray.load_dataset(f).compute()
 
@@ -198,100 +381,13 @@ def main():
     ) = data_utils.extract_inputs_targets_forcings(
         example_batch,
         target_lead_times=slice("6h", f"{eval_steps*6}h"),
-        **dataclasses.asdict(task_config),
+        **dataclasses.asdict(model.task_config),
     )
 
-    print("All Examples:  ", example_batch.dims.mapping)
-    print("Eval Inputs:   ", eval_inputs.dims.mapping)
-    print("Eval Targets:  ", eval_targets.dims.mapping)
-    print("Eval Forcings: ", eval_forcings.dims.mapping)
-
-    # run autoregression
-    assert model_config.resolution in (0, 360.0 / eval_inputs.sizes["lon"]), (
-        "Model resolution doesn't match the data resolution. You likely want to "
-        "re-filter the dataset list, and download the correct data."
-    )
-
-    print("Inputs:  ", eval_inputs.dims.mapping)
-    print("Targets: ", eval_targets.dims.mapping)
-    print("Forcings:", eval_forcings.dims.mapping)
-
-    # load stats
-    with open(os.path.join(root, "stats/diffs_stddev_by_level.nc"), "rb") as f:
-        diffs_stddev_by_level = xarray.load_dataset(f).compute()
-    with open(os.path.join(root, "stats/mean_by_level.nc"), "rb") as f:
-        mean_by_level = xarray.load_dataset(f).compute()
-    with open(os.path.join(root, "stats/stddev_by_level.nc"), "rb") as f:
-        stddev_by_level = xarray.load_dataset(f).compute()
-
-    # jit the stuff
-    # @title Build jitted functions, and possibly initialize random weights
-    def construct_wrapped_graphcast(
-        model_config: graphcast.ModelConfig, task_config: graphcast.TaskConfig
-    ):
-        """Constructs and wraps the GraphCast Predictor."""
-        # Deeper one-step predictor.
-        predictor = graphcast.GraphCast(model_config, task_config)
-
-        # Modify inputs/outputs to `graphcast.GraphCast` to handle conversion to
-        # from/to float32 to/from BFloat16
-        predictor = casting.Bfloat16Cast(predictor)
-
-        # Modify inputs/outputs to `casting.Bfloat16Cast` so the casting to/from
-        # BFloat16 happens after applying normalization to the inputs/targets.
-        predictor = normalization.InputsAndResiduals(
-            predictor,
-            diffs_stddev_by_level=diffs_stddev_by_level,
-            mean_by_level=mean_by_level,
-            stddev_by_level=stddev_by_level,
-        )
-
-        # Wraps everything so the one-step model can produce trajectories.
-        predictor = autoregressive.Predictor(predictor, gradient_checkpointing=True)
-        return predictor
-
-    @hk.transform_with_state
-    def run_forward(model_config, task_config, inputs, targets_template, forcings):
-        predictor = construct_wrapped_graphcast(model_config, task_config)
-        return predictor(inputs, targets_template=targets_template, forcings=forcings)
-
-    # Jax doesn't seem to like passing configs as args through the jit. Passing it
-    # in via partial (instead of capture by closure) forces jax to invalidate the
-    # jit cache if you change configs.
-    def with_configs(fn):
-        return functools.partial(fn, model_config=model_config, task_config=task_config)
-
-    # Always pass params and state, so the usage below are simpler
-    def with_params(fn):
-        return functools.partial(fn, params=params, state=state)
-
-    # Our models aren't stateful, so the state is always empty, so just return the
-    # predictions. This is requiredy by our rollout code, and generally simpler.
-    def drop_state(fn):
-        return lambda **kw: fn(**kw)[0]
-
-    init_jitted = jax.jit(with_configs(run_forward.init))
-
-    if params is None:
-        params, state = init_jitted(
-            rng=jax.random.PRNGKey(0),
-            inputs=eval_inputs,
-            targets_template=eval_targets,
-            forcings=eval_forcings,
-        )
-
-    run_forward_jitted = drop_state(
-        with_params(jax.jit(with_configs(run_forward.apply)))
-    )
-
-    predictions = rollout.chunked_prediction(
-        run_forward_jitted,
-        rng=jax.random.PRNGKey(0),
-        inputs=eval_inputs,
-        targets_template=eval_targets * np.nan,
-        forcings=eval_forcings,
-    )
-
+    eval_inputs.info()
+    eval_targets.info()
+    eval_forcings.info()
+    predictions = model(eval_inputs, eval_targets, eval_forcings)
     print(predictions)
 
 
