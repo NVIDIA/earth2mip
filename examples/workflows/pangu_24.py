3a3be25d8565973a120756411a9bc5562df6ebcb
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2023-09-13 15:41:49 -0700

    fix: fixing workflow scripts (#25)

--- examples/workflows/pangu_24.py
+++ examples/workflows/pangu_24.py
@@ -15,41 +15,52 @@
 # limitations under the License.
 
 # %%
+import os
 import numpy as np
 import datetime
+
+# Set number of GPUs to use to 1
+os.environ["WORLD_SIZE"] = "1"
+# Set model registry as a local folder
+model_registry = os.path.join(os.path.dirname(os.path.realpath(os.getcwd())), "models")
+os.makedirs(model_registry, exist_ok=True)
+os.environ["MODEL_REGISTRY"] = model_registry
+
+import earth2mip.networks.pangu as pangu
 from earth2mip import (
     registry,
     inference_ensemble,
 )
-from earth2mip.networks.pangu import load_24substep6
 from earth2mip.initial_conditions import cds
+from os.path import dirname, abspath, join
 
-# %%
-
-package = registry.get_model("pangu_weather_24substep6")
-
-timeloop = load_24substep6(package, device="cuda:0")
-# path = sys.argv[1]
-# data_source = HDF5DataSource.from_path(path)
-data_source = cds.DataSource(timeloop.in_channel_names)
-output = "path/"
+# %% Load model package and data source
+print("Loading pangu model, this can take a bit")
+package = registry.get_model("pangu")
+# Load just the 24 hour model (also supports 6 hour)
+inferener = pangu.load_single_model(package, time_step_hours=24)
+data_source = cds.DataSource(inferener.in_channel_names)
 time = datetime.datetime(2018, 1, 1)
+
+# %% Run inference
 ds = inference_ensemble.run_basic_inference(
-    timeloop,
+    inferener,
     n=12,
     data_source=data_source,
     time=time,
 )
 print(ds)
 
-
-# %%
+# %% Post-process
+import matplotlib.pyplot as plt
 from scipy.signal import periodogram
+
+output = f"{dirname(dirname(abspath(__file__)))}/outputs/workflows"
+os.makedirs(output, exist_ok=True)
 
 arr = ds.sel(channel="u200").values
 f, pw = periodogram(arr, axis=-1, fs=1)
 pw = pw.mean(axis=(1, 2))
-import matplotlib.pyplot as plt
 
 l = ds.time - ds.time[0]  # noqa
 days = l / (ds.time[-1] - ds.time[0])
@@ -61,9 +72,9 @@ for k in range(ds.sizes["time"]):
 plt.legend()
 plt.ylim(bottom=1e-8)
 plt.grid()
-plt.savefig("u200_spectra.png")
+plt.savefig(join(output, "u200_spectra_pangu24.png"), bbox_inches="tight")
 
 # %%
 day = (ds.time - ds.time[0]) / np.timedelta64(1, "D")
 plt.semilogy(day, pw[:, 100:].mean(-1), "o-")
-plt.savefig("u200_high_wave.png")
+plt.savefig(join(output, "u200_high_wave_pangu24.png"), bbox_inches="tight")
