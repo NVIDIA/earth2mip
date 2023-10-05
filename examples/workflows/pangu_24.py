2a94d0b350279fa6bc68b7eb101d749eac1b9d1c
Nick Geneva <nicholasgeneva@gmail.com>
2023-10-05 00:19:23 +0000

    Format?

--- examples/workflows/pangu_24.py
+++ examples/workflows/pangu_24.py
@@ -18,6 +18,7 @@
 import os
 import numpy as np
 import datetime
+import json
 
 # Set number of GPUs to use to 1
 os.environ["WORLD_SIZE"] = "1"
@@ -28,16 +29,41 @@ os.environ["MODEL_REGISTRY"] = model_registry
 
 # %% Download Pangu checkpoints
 import subprocess
-if not os.path.isdir(os.path.join(model_registry, 'pangu')):
+
+if not os.path.isdir(os.path.join(model_registry, "pangu")):
     pangu_registry = os.path.join(model_registry, "pangu")
     os.makedirs(pangu_registry, exist_ok=True)
     # Wget onnx files
     print("Downloading model checkpoint, this may take a bit")
-    subprocess.run(['wget', '-nc', '-P', f'{pangu_registry}', 'https://get.ecmwf.int/repository/test-data/ai-models/pangu-weather/pangu_weather_24.onnx'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    subprocess.run(['wget', '-nc', '-P', f'{pangu_registry}', 'https://get.ecmwf.int/repository/test-data/ai-models/pangu-weather/pangu_weather_6.onnx'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.run(
+        [
+            "wget",
+            "-nc",
+            "-P",
+            f"{pangu_registry}",
+            "https://get.ecmwf.int/repository/test-data/ai-models"
+            + "/pangu-weather/pangu_weather_24.onnx",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+    )
+    subprocess.run(
+        [
+            "wget",
+            "-nc",
+            "-P",
+            f"{pangu_registry}",
+            "https://get.ecmwf.int/repository/test-data/ai-models"
+            + "/pangu-weather/pangu_weather_6.onnx",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+    )
 
-    with open(os.path.join(pangu_registry, 'metadata.json'), 'w') as outfile:
-        json.dump({"entrypoint": {"name": "earth2mip.networks.pangu:load"}}, outfile, indent=2)
+    with open(os.path.join(pangu_registry, "metadata.json"), "w") as outfile:
+        json.dump(
+            {"entrypoint": {"name": "earth2mip.networks.pangu:load"}}, outfile, indent=2
+        )
 
 
 import earth2mip.networks.pangu as pangu
