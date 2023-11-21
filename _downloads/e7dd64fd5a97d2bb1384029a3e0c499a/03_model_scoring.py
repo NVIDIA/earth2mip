a103b73587b257ad1e375b8c9bc22a8a29ad0b9f
Nick Geneva <nicholasgeneva@gmail.com>
2023-11-20 23:07:39 -0800

    Examples Update: 4083747cf8240fc33883243818b241e8d375c0a7

--- _downloads/e7dd64fd5a97d2bb1384029a3e0c499a/03_model_scoring.py
+++ _downloads/e7dd64fd5a97d2bb1384029a3e0c499a/03_model_scoring.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# %% [markdown]
+# %%
 """
 Scoring Models
 ==============
@@ -36,61 +36,51 @@ In summary this notebook will cover the following topics:
 import datetime
 import os
 
-# %% [markdown]
-"""
-## Setting up HDF5 data
+# %%
+# Setting up HDF5 data
+# ---------------------
+# The first step of scoring is handling the target data. One could simply use the
+# CDSDatasource to download target data on the fly, but depending on how comprehensive
+# the scoring is, this can prove quite slow.
+# Additionally, many scoring pipelines require on-prem data.
+# Thus, this will demonstrate how to use the HDF5 datasource.
+# The HDF5 data source assumes that the data to be loaded is stored in the general form:
+# ::
+#
+#   year.h5
+#       | - field (time, channels, grid)
+#
+# For AFNO which requires 34 channels with a time-step size of 6 hours, an H5 file will
+# have the following form of data:
+# ::
+#
+#   2017.h5
+#       | - field (1460, 34, 720, 1440)
+#   2016.h5
+#       | - field (1464, 34, 720, 1440)
+#   2015.h5
+#       | - field (1460, 34, 720, 1440)
+#
+# (Note the later two dims have some flexibility with regridding)
+# One option to build these H5 files from scratch is to use the ERA5 mirror scripts
+# provided in `Modulus <https://github.com/NVIDIA/modulus/tree/main/examples/weather/dataset_download>`_.
+# For the rest of this tutorial, it is assumed that 2017.h5 is present for the full year.
+# %%
+import dotenv
 
-The first step of scoring is handling the target data. One could simply use the
-CDSDatasource to download target data on the fly, but depending on how comprehensive
-the scoring is, this can prove quite slow.
-Additionally, many scoring pipelines require on-prem data.
-Thus, this will demonstrate how to use the HDF5 datasource.
-
-The HDF5 data source assumes that the data to be loaded is stored in the general form:
-
-year.h5
- | - field (time, channels, grid)
-
-For AFNO which requires 34 channels with a time-step size of 6 hours, an H5 file will
-have the following form of data:
-
-2017.h5
- | - field (1460, 34, 720, 1440)
-2016.h5
- | - field (1464, 34, 720, 1440)
-2015.h5
- | - field (1460, 34, 720, 1440)
-
-(Note the later two dims have some flexibility with regridding)
-
-One option to build these H5 files from scratch is to use the ERA5 mirror scripts
-provided in [Modulus](https://github.com/NVIDIA/modulus/tree/main/examples/weather/dataset_download).
-For the rest of this tutorial, it is assumed that 2017.h5 is present for the full year.
-"""  # noqa: E501
+dotenv.load_dotenv()
+# can set this with the export ERA5_HDF5=/path/to/root/of/h5/files
+h5_folder = os.getenv("ERA5_HDF5")
 
 # %%
-# load an values in a .env file where you can specify your H5 root folder by
-# adding a line like:
-try:
-    import dotenv
+# Loading Models
+# --------------
+# With the HDF5 datasource loaded the next step is to load our model we wish to score.
+# In this tutorial we will be using the built in FourcastNet model.
+# Take note of the `e2mip://` which will direct Earth-2 MIP to load a known model package.
+# FourcastNet is selected here simply because its a 34 channel model which aligns with the
+# H5 files described above.
 
-    dotenv.load_dotenv()
-except ModuleNotFoundError:
-    pass  # pip install python-dotenv
-
-# can set this with the export ERA5_HDF5=/path/to/root/of/h5/files
-h5_folder = os.getenv("ERA5_HDF5", "/mount/73vars")
-
-# %% [markdown]
-"""
-## Loading Models
-
-With the HDF5 datasource loaded the next step is to load our model we wish to score.
-In this tutorial we will be using the built in FourcastNet model.
-Take note of the `e2mip://` which will direct Earth-2 MIP to load a known model package.
-FourcastNet is selected here simply because its a 34 channel model which aligns with the
-H5 files described above.
-"""
 # %%
 from modulus.distributed import DistributedManager
 
@@ -101,18 +91,17 @@ device = DistributedManager().device
 package = registry.get_model("e2mip://dlwp")
 model = dlwp.load(package, device=device)
 
-# %% [markdown]
-"""
-## HDF5 Datasource
+# %%
+# HDF5 Datasource
+# ---------------
+# With the H5 files properly formatted, a H5 datasource can now get defined.
+# This requires two items: a root directory location of the H5 files as well as some
+# metadata. Se
+# The metadata is a JSON/dictionary object that helps Earth-2 MIP index the H5 file.
+# Typically, this can be done by placing a `data.json` file next to the H5 files.
+# See `this documentation <https://github.com/NVIDIA/earth2mip/blob/f44c580ccc3d98bf349fe97823bb1540e532c80d/earth2mip/initial_conditions/hdf5.py#L38>`_
+# for more details on how to set up input data correctly.
 
-With the H5 files properly formatted, a H5 datasource can now get defined.
-This requires two items: a root directory location of the H5 files as well as some
-metadata. Se
-The metadata is a JSON/dictionary object that helps Earth-2 MIP index the H5 file.
-Typically, this can be done by placing a `data.json` file next to the H5 files.
-See [this documentation](https://github.com/NVIDIA/earth2mip/blob/f44c580ccc3d98bf349fe97823bb1540e532c80d/earth2mip/initial_conditions/hdf5.py#L38)
-for more details on how to set up input data correctly.
-"""  # noqa
 # %%
 from earth2mip.initial_conditions import hdf5
 
@@ -125,20 +114,18 @@ time = datetime.datetime(2017, 5, 1, 18)
 out = datasource[time]
 print(out.shape)
 
-# %% [markdown]
-"""
-## Running Scoring
-
-With the datasource and model loaded, scoring can now be performed.
-To score this we will run 10 day forecasts over the span of the entire year at 30 day
-intervals.
-For research, one would typically want this to be much more comprehensive so feel free
-to customize for you're use case.
-
-The `score_deterministic` API provides a simple way to calculate RMSE and ACC scores.
-ACC scores require climatology which is beyond the scope of this example, thus zero
-values will be provided and only the RMSE will be of concern.
-"""
+# %%
+# Running Scoring
+# ---------------
+# With the datasource and model loaded, scoring can now be performed.
+# To score this we will run 10 day forecasts over the span of the entire year at 30 day
+# intervals.
+# For research, one would typically want this to be much more comprehensive so feel free
+# to customize for you're use case.
+#
+# The `score_deterministic` API provides a simple way to calculate RMSE and ACC scores.
+# ACC scores require climatology which is beyond the scope of this example, thus zero
+# values will be provided and only the RMSE will be of concern.
 
 # %%
 from earth2mip.inference_medium_range import save_scores, time_average_metrics
@@ -159,15 +146,12 @@ if not os.path.exists(output_dir):
         output_directory=output_dir,
     )
 
-# %% [markdown]
-"""
-## Post Processing
-
-The last step is any post processing / IO that is desired.
-Typically its recommended to save the output dataset to a netCDF file for further
-processing.
-Lets plot the RMSE of the z500 (geopotential at pressure level 500) field.
-"""
+# %%
+# Post Processing
+# ---------------
+# The last step is any post processing / IO that is desired.
+# Typically its recommended to save the output dataset to a netCDF file for further processing.
+# Lets plot the RMSE of the z500 (geopotential at pressure level 500) field.
 
 # %%
 import matplotlib.pyplot as plt
@@ -189,8 +173,6 @@ ax.set_title("DLWP z500 RMSE 2017")
 plt.savefig(f"{output_dir}/dwlp_z500_rmse.png")
 
 
-# %% [markdown]
-"""
-This completes this introductory notebook on basic scoring of models in Earth-2 MIP,
-which is founational for comparing the performance of different models.
-"""
+# %%
+# This completes this introductory notebook on basic scoring of models in Earth-2 MIP,
+# which is founational for comparing the performance of different models.
