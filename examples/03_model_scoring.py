ff534458a60795f8e1b0f1a1a46489a579ee0438
Nicholas Geneva <ngeneva@nvidia.com>
2023-11-28 06:38:13 +0000

    Fixing example 3 docs

--- examples/03_model_scoring.py
+++ examples/03_model_scoring.py
@@ -50,21 +50,26 @@ import os
 #   year.h5
 #       | - field (time, channels, grid)
 #
-# For AFNO which requires 34 channels with a time-step size of 6 hours, an H5 file will
+# For DLWP which requires 7 channels with a time-step size of 12 hours, an H5 file will
 # have the following form of data:
 # ::
 #
 #   2017.h5
-#       | - field (1460, 34, 720, 1440)
+#       | - field (1460, 7, 720, 1440)
 #   2016.h5
-#       | - field (1464, 34, 720, 1440)
+#       | - field (1464, 7, 720, 1440)
 #   2015.h5
-#       | - field (1460, 34, 720, 1440)
+#       | - field (1460, 7, 720, 1440)
 #
-# (Note the later two dims have some flexibility with regridding)
+# .. note::
+#   There is some flexibility with the dimensions of the data in the H5 files. The
+#   fields may contain additional channels not needed by the model. Additionally, the
+#   later two dims have some flexibility with regridding.
+#
 # One option to build these H5 files from scratch is to use the ERA5 mirror scripts
 # provided in `Modulus <https://github.com/NVIDIA/modulus/tree/main/examples/weather/dataset_download>`_.
 # For the rest of this tutorial, it is assumed that 2017.h5 is present for the full year.
+
 # %%
 import dotenv
 
@@ -126,6 +131,8 @@ print(out.shape)
 # The `score_deterministic` API provides a simple way to calculate RMSE and ACC scores.
 # ACC scores require climatology which is beyond the scope of this example, thus zero
 # values will be provided and only the RMSE will be of concern.
+# This function will save the results of every inference run into a CSV file which
+# can then be pose process using some of the utility functions Earth-2 MIP provides.
 
 # %%
 from earth2mip.inference_medium_range import save_scores, time_average_metrics
