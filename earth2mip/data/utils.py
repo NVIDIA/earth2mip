893dd3b0d4e6f589b4e89f596e5337a3b22f0bd1
Nicholas Geneva <ngeneva@nvidia.com>
2023-12-05 02:05:42 +0000

    Adding small util for converting out of dataarrays

---
+++ earth2mip/data/utils.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+from typing import Optional
+
+import numpy as np
+import torch
+import xarray as xr
+
+
+def prep_data_array(
+    da: xr.DataArray, device: Optional[torch.device] = "cpu"
+) -> tuple[torch.Tensor, OrderedDict[str, np.ndarray]]:
+    """Prepares a data array from a data source for inference workflows by converting
+    the data array to a torch tensor and the coordinate system to an OrderedDict.
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        Input data array
+    device : torch.device, optional
+        Torch devive to load data tensor to, by default "cuda:0"
+
+    Returns
+    -------
+    tuple[torch.Tensor, OrderedDict[str, np.ndarray]]
+        Tuple containing output tensor and coordinate OrderedDict
+    """
+    out = torch.Tensor(da.values).to(device)
+
+    out_coords = OrderedDict()
+    for dim in da.coords.dims:
+        out_coords[dim] = np.array(da.coords[dim])
+
+    return out, out_coords
---
+++ test/data/test_utils.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+import numpy as np
+import pytest
+import torch
+import xarray as xr
+
+from earth2mip.data.utils import prep_data_array
+
+
+@pytest.fixture
+def foo_data_array():
+    time0 = datetime.datetime.now()
+    return xr.DataArray(
+        data=np.random.rand(8, 16, 32),
+        dims=["one", "two", "three"],
+        coords={
+            "one": [time0 + i * datetime.timedelta(hours=6) for i in range(8)],
+            "two": [f"{i}" for i in range(16)],
+            "three": np.linspace(0, 1, 32),
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "device",
+    [
+        "cpu",
+        pytest.param(
+            "cuda:0",
+            marks=pytest.mark.skipif(
+                not torch.cuda.is_available(), reason="cuda missing"
+            ),
+        ),
+    ],
+)
+@pytest.mark.parametrize("dims", [["one", "two", "three"], ["three", "one", "two"]])
+def test_prep_dataarray(foo_data_array, dims, device):
+
+    data_array = foo_data_array.transpose(*dims)
+    out, outc = prep_data_array(data_array, device)
+
+    assert str(out.device) == device
+    assert list(outc.keys()) == list(data_array.dims)
+    for key in outc.keys():
+        assert (outc[key] == np.array(data_array.coords[key])).all()
+    assert out.shape == data_array.data.shape
