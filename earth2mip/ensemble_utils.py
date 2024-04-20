eb25bdf2c8ddf7a9da279a96f22c4b639c8f5067
ankurmahesh <mahesh.ankur10@gmail.com>
2024-04-19 17:24:04 -0700

    removing unused code for Inference class, ensemble_utils, and inference_ensemble

--- earth2mip/ensemble_utils.py
+++ earth2mip/ensemble_utils.py
@@ -203,31 +203,14 @@ def generate_bred_vector_timeevolve(
     )
 
     optimization_target = _load_optimal_targets('sfno_linear_73chq_sc3_layers8_edim384_wstgl2', 48, model.channel_names).to(x.device).squeeze().to(torch.float32) * 0.35
-    #optimization_target = 0.2 * torch.from_numpy(np.load("/pscratch/sd/p/pharring/74var-6hourly/staging/stats_fcndev/time_diff_stds.npy")).to(x.device).squeeze().to(torch.float32)
-    #optimization_target = graphcast_weights(model.channel_names, model.scale, "deterministic rmse").to(x.device).squeeze().to(torch.float32)
-    #time_means = torch.from_numpy(np.load("/pscratch/sd/p/pharring/74var-6hourly/staging/stats_fcndev/time_means.npy")).to(x.device).to(torch.float32)
-    #optimization_target = calculate_global_geometric_mean(time_means.squeeze()) * 0.05
     sampler = CorrelatedSphericalField(720, 500. * 1000, 48.0, 1.0, N=74).to(
         x.device
     )
     correlated_random_noise = sampler()
-    #correlated_random_noise = torch.cat([correlated_random_noise] * 74, dim=1)
-    original_pert = correlated_random_noise.clone().detach()
     dx = optimization_target[:, None, None] * correlated_random_noise
-    #dx = model.scale * correlated_random_noise * 0.05
-    #dx = (correlated_random_noise * 0.05) * x[0]
     #channel_idx = set(range(len(model.channel_names))) - set([model.channel_names.index('z500')])
     channel_idx = list(range(len(model.channel_names)))
     channel_idx.remove(model.channel_names.index('z500'))
-    #channel_idx = []
-    #for idx, channel in enumerate(model.channel_names):
-    #    #if channel in ['q1000','q850']:
-    #    if channel in ["z50", "z100", "z150", "z200", "z250", "z300", "z400", "z500"]: 
-    #        #these channels will be perturbed
-    #        pass
-    #    else:
-    #        #these channels will not be perturbed
-    #        channel_idx.append(idx)
     dx[ :, list(channel_idx), : 721, : 1440] = 0
 
     data_source = initial_conditions.get_data_source(
@@ -237,13 +220,6 @@ def generate_bred_vector_timeevolve(
     )
 
     date_obj = weather_event.properties.start_time
-    #lag = np.random.choice(np.arange(0,49,6)).astype(float)
-    #lagged_time = date_obj - timedelta(hours=lag)
-    #lagged_ic = initial_conditions.get_initial_condition_for_model(model, data_source, lagged_time)
-    #exaggerate_factor = calculate_global_rms(lagged_ic - x) / optimization_target[None, None]
-    #dx = dx / exaggerate_factor[:, :, :, None, None]
-    #return (lagged_ic - x) / model.scale / 50
-
     for step in list(range(integration_steps))[::-1]:
         x0 = initial_conditions.get_initial_condition_for_model(model, data_source, date_obj - (timedelta(hours=6*step)))
 
@@ -271,44 +247,11 @@ def generate_bred_vector_timeevolve(
         if inflate:
             dx += noise_amplitude * (dx - dx.mean(dim=0)) * model.scale
 
-        #exaggerate_factor = calculate_global_rms(dx) / optimization_target[None, None]
-        #dx = dx / exaggerate_factor[:, :, :, None, None]
-
-        #exaggerate_factor = calculate_global_geometric_mean(dx) / optimization_target[None, None]
-        #dx = dx / exaggerate_factor[:, :, :, None, None]
-
         exaggerate_factor = hemispheric_rms(dx) / optimization_target[None, None, :, None]
         dx = dx / exaggerate_factor[:, :, :, : , None]
 
         #dx = set_50hPa_to_0(dx, model.channel_names)
         
-        #sampler = CorrelatedSphericalField(720, 2000. * 1000, 48.0, 0.005, N=1).to(
-        #    x.device
-        #)
-        #correlated_random_noise = sampler()
-        #correlated_random_noise = torch.cat([correlated_random_noise] * 74, dim=1)
-        #channels_to_exclude = []
-        #for idx, channel in enumerate(model.channel_names):
-        #    #if channel in ['z500']: 
-        #    if channel in ["z50", "z100", "z150", "z200", "z250", "z300", "z400", "z500"]: 
-        #        #these channels will be perturbed
-        #        pass
-        #    else:
-        #        #these channels will not be perturbed
-        #        channels_to_exclude.append(idx)
-        #correlated_random_noise[:, torch.Tensor(np.asarray(channels_to_exclude)).to(torch.int), : 721, : 1440] = 0
-        #dx = dx + (x2 * correlated_random_noise)
-        #dx = dx + (optimization_target[:, None, None] * correlated_random_noise)
-    
-    #dx = dx + original_pert
-    #optimization_target = optimization_target[None, None]
-    #exaggerate_factor = calculate_global_rms(dx) / optimization_target
-    #dx = dx / exaggerate_factor[:, :, :, None, None]
-
-    #optimization_target = optimization_target[None, None, :, None]
-    #exaggerate_factor = hemispheric_rms(dx) / optimization_target
-    #dx = dx / exaggerate_factor[:, :, :, : , None]
-
     return dx / model.scale
 
 def set_50hPa_to_0(dx, channel_names):
@@ -396,18 +339,6 @@ def generate_bred_vector(
 
     return dx / model.scale 
 
-def calculate_global_geometric_mean(tensor):
-    jacobian = torch.sin(
-            torch.linspace(0, torch.pi,tensor.shape[-2])
-        ).unsqueeze(1).to(tensor.device)
-    dtheta = torch.pi / tensor.shape[-2]
-    dlambda = 2 * torch.pi / tensor.shape[-1]
-    dA = dlambda * dtheta
-    quad_weight = dA * jacobian
-
-    weights = quad_weight / quad_weight.mean()
-    return (tensor.abs() * weights).log().mean(dim=(-1,-2)).exp()
-
 def calculate_global_rms(tensor):
     jacobian = torch.sin(
             torch.linspace(0, torch.pi,tensor.shape[-2])
@@ -446,74 +377,17 @@ def calculate_sh_rms(tensor):
     weights = quad_weight / quad_weight.mean()
     return torch.sqrt(torch.mean(tensor[:, :, :, -280:]**2 * weights[-280:], dim=(-1,-2)))
 
-def calculate_tropics_rms(tensor):
-    jacobian = torch.sin(
-            torch.linspace(0, torch.pi,tensor.shape[-2])
-        ).unsqueeze(1).to(tensor.device)
-    dtheta = torch.pi / tensor.shape[-2]
-    dlambda = 2 * torch.pi / tensor.shape[-1]
-    dA = dlambda * dtheta
-    quad_weight = dA * jacobian
-
-    weights = quad_weight / quad_weight.mean()
-    return torch.sqrt(torch.mean(tensor[:, :, :, 320:-320]**2 * weights[320:-320], dim=(-1,-2)))
-
 def hemispheric_rms(tensor):
     nh = calculate_nh_rms(tensor).squeeze() 
     sh = calculate_sh_rms(tensor).squeeze()
-    t = calculate_tropics_rms(tensor).squeeze()
     hemispheric_rmse = torch.zeros(74, 721, device=sh.device)
     for i in range(74):
         nh_arr = torch.full([280], nh[i])
         sh_arr = torch.full([280], sh[i])
-        #tropics = torch.full([161], (nh[i] + sh[i])/2)
         tropics = torch.linspace(nh[i], sh[i], 721 - nh_arr.shape[0]*2) 
-        #nh_arr = torch.linspace(nh[i], tropics[0], 240)
-        #sh_arr = torch.linspace(tropics[-1], sh[i], 240)
-        #tropics = torch.full([161], t[i])
         hemispheric_rmse[i] = torch.cat([nh_arr, tropics, sh_arr])
 
     return hemispheric_rmse[None, None] 
-
-def graphcast_weights(channel_names, global_stds, loss_type='squared temp-std'):
-    channel_weights = torch.ones(len(channel_names), dtype=torch.float32, device=global_stds.device)
-    for c, chn in enumerate(channel_names):
-        if chn in ["u10m", "v10m", "u100m", "v100m", "tp", "sp", "msl"]:
-            channel_weights[c] = 0.1
-        elif chn in ["tcwv"]:
-            channel_weights[c] = 0.3
-        elif chn in ["t2m", "2d", "d2m"]:
-            channel_weights[c] = 1.0
-        elif chn[0] in ["z", "u", "v", "t", "r", "q"]:
-            pressure_level = float(chn[1:])
-            channel_weights[c] = 0.001 * pressure_level
-        else:
-            channel_weights[c] = 0.01
-
-    # renormalize the weights to one
-    channel_weights = channel_weights.reshape(1, -1, 1, 1)
-    #channel_weights = channel_weights / torch.sum(channel_weights)
-
-    if "squared" in loss_type:
-        squared = True
-    else:
-        squared = False
-
-    target =  _load_optimal_targets("", 24, channel_names).squeeze().to(channel_weights.device) 
-    if "temp-std" in loss_type:
-        eps = 1e-6
-        global_stds = global_stds.reshape(1, -1, 1, 1)
-        dt = 1
-        time_diff_stds = np.sqrt(dt) * torch.from_numpy(np.load("/pscratch/sd/p/pharring/74var-6hourly/staging/stats_fcndev/time_diff_stds.npy")).reshape(1, -1, 1, 1).to(global_stds.device)
-        time_var_weights = global_stds / (time_diff_stds + eps)
-        # time_var_weights = 1 / (time_diff_stds+eps)
-        if squared:
-            time_var_weights = time_var_weights**2
-            #target = target**2
-        channel_weights = channel_weights * time_var_weights
-    if "deterministic rmse" in loss_type:
-        channel_weights = channel_weights.squeeze() * target
-    return channel_weights
 
 def _load_optimal_targets(config_model_name, lead_time, channel_names):
     from earth2mip import forecast_metrics_io
@@ -521,86 +395,12 @@ def _load_optimal_targets(config_model_name, lead_time, channel_names):
     targets = []
     #return torch.from_numpy(sfno['value'].sel(channel=channel_names).sel(lead_time=lead_time).values[np.newaxis])
     for name in channel_names:
-        if name[0] in ['u', 'v', 't', 'q', 'd', '2']:
-            lead_time = lead_time
-        else:
-            lead_time = lead_time
         targets.append(sfno['value'].sel(channel=name, lead_time=lead_time).values)
         #if name == 'z50':
         #    targets.append(sfno['value'].sel(channel='z100', lead_time=lead_time).values)
         #else:
         #    targets.append(sfno['value'].sel(channel=name, lead_time=lead_time).values)
     return torch.Tensor(np.asarray(targets))[None]
-
-
-
-def generate_model_noise_correlated(
-    x,
-    time,
-    scale,
-    reddening,
-    noise_injection_amplitude,
-):
-    noise = noise_injection_amplitude * brown_noise(x.shape, reddening).to(x.device)
-    channels = list(set(range(73)) - set([41]))
-    noise[:, :, channels, : 721, : 1440] = 0
-    return noise * scale / 21600
-
-def generate_multiplicative_noise(x, time, length_scale, channel_names, sht, ivsht, weighing_method="zero", eps=50, std=0.18, center=1):
-    """                                                                 
-    Inputs                                                              
-                                                                        
-        length_scale (int): length scale in km of spherical random noise
-        weighing_method (str) : how to weigh the wavelengths outside of the weighing method
-                                "zero" to zero them out                 
-                                "gaussian" for gaussian weighting with mean 0 and sd 0.01
-                                "inverse" for weighing them by their inverse distance from length_scale
-        eps (int).        : the number of km plus or minus length scale to provide full weight to
-                                                                        
-    """                                                                 
-    shape = (x.shape[-2], x.shape[-1])
-    device = x.device
-    rand_noise = torch.randn(*shape, device=device)                     
-    nlat, nlon = rand_noise.shape                                       
-                                                                        
-    coeffs = sht(rand_noise)                                            
-    r_earth = 6.371e6                                                   
-                                                                        
-    l = torch.arange(nlat, device=device)
-    # convert spherical harmonic degree to wavelength [km]              
-    lam = 2*np.pi*r_earth/(l + 1) / 1e3                                 
-                                                                        
-    #How to weigh the wavelengths not in the range of length scale      
-    if weighing_method == "gaussian":                                   
-        weights = np.random.normal(0, 0.01, size=coeffs.shape)          
-    elif weighing_method == "zero":                                     
-        weights = torch.zeros(coeffs.shape, device=device) 
-    else:                                                               
-        assert NotImplemented                                           
-
-    #epsilon
-    weights[torch.logical_and(lam >= length_scale - eps, lam < length_scale + eps), : ] = 1
-    coeffs_weighted = coeffs * weights
-
-    correlated_noise = ivsht(coeffs_weighted)
-    correlated_noise = (correlated_noise - correlated_noise.mean())/(correlated_noise.std()) * std
-    #if grid != schema.Grid.grid_721x1440:
-    #    correlated_noise = correlated_noise[0:-1, :]  # 720 x 1440
-    
-    num_channels = len(channel_names)
-    channels_to_exclude = []
-    stacked_noise = torch.stack([correlated_noise]*num_channels, dim=0)[None].to(torch.float32)
-    for idx, channel in enumerate(channel_names):
-        if channel[0] in ['u', 'v', 't', 'q'] and channel[1:].isdigit():
-            pass
-        else:
-            channels_to_exclude.append(idx)
-    
-    print('model tendency perturbed')
-    stacked_noise[:, channels_to_exclude, : 721, : 1440] = 0
-    stacked_noise = torch.where(stacked_noise > 1, 1, stacked_noise)
-    stacked_noise = torch.where(stacked_noise < -1, -1, stacked_noise)
-    return stacked_noise + center
 
 class CorrelatedSphericalField(torch.nn.Module):
     def __init__(
@@ -727,26 +527,6 @@ class CorrelatedSphericalField(torch.nn.Module):
         xi = torch.view_as_complex(xi)
 
         self.coeff = (self.phi * self.coeff) + (self.sigma_n * xi)
-        if x is not None:
-            num_channels = len(self.channel_names)
-            channels_to_exclude = []
-            noise = torch.cat([noise]*num_channels,dim=1)
-            for idx, channel in enumerate(self.channel_names):
-                #if channel[0] in ['u', 'v', 't', 'q'] and channel[1:].isdigit():
-                if channel in ['2d', 'd2m'] or (channel[0] in ['q'] and channel[1:].isdigit() and int(channel[1:]) > 800):
-                    #these channels will be perturbed
-                    pass
-                else:
-                    #these channels will not be perturbed
-                    channels_to_exclude.append(idx)
-            
-            print('model tendency perturbed: all channels except: {}'.format(np.asarray(self.channel_names)[channels_to_exclude]))
-            #noise[:, torch.Tensor(np.asarray(channels_to_exclude)).to(torch.int), : 721, : 1440] = 0
-            #noise= torch.where(noise > 1, 1, noise)
-            #noise = torch.where(noise < -1, -1, noise)
-            noise = noise + 1
-            #stacked_noise = stacked_noise + 1
-            #noise = stacked_noise
         return noise
 
     # Override cuda and to methods so sampler gets initialized with mean
--- earth2mip/networks/__init__.py
+++ earth2mip/networks/__init__.py
@@ -202,7 +202,6 @@ class Inference(torch.nn.Module, time_loop.TimeLoop):
         x: torch.Tensor,
         restart: Optional[Any] = None,
         normalize=True,
-        model_perturb=True,
     ) -> Iterator[Tuple[datetime.datetime, torch.Tensor, Any]]:
         """
         Args:
@@ -222,9 +221,9 @@ class Inference(torch.nn.Module, time_loop.TimeLoop):
         if restart:
             yield from self._iterate(**restart)
         else:
-            yield from self._iterate(x=x, time=time, model_perturb=model_perturb)
+            yield from self._iterate(x=x, time=time)
 
-    def _iterate(self, x, model_perturb, normalize=True, time=None):
+    def _iterate(self, x, normalize=True, time=None):
         """Yield (time, unnormalized data, restart) tuples
 
         restart = (time, unnormalized data)
@@ -246,55 +245,12 @@ class Inference(torch.nn.Module, time_loop.TimeLoop):
             restart = dict(x=x, normalize=False, time=time)
             yield time, self.scale * x[:, -1] + self.center, restart
             
-            old = x.clone().detach()
-            idx = -1
             while True:
-                idx += 1
-                if False and model_perturb and idx >= 4:
-                    raise NotImplementedError("STTP model perturbation is still under development")
-                    if len(self.all_models) == 0:
-                        model_names = [
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed26",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed27",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed28",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed29",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed30",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed31",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed70",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed71",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed72",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed74",
-                                "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed76",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed12",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed16",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed17",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed18",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed77",
-                            "sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed78",
-                        ]
-                        model_indices = np.random.choice(len(model_names),4,replace=False)
-                        for model_name in np.asarray(model_names)[model_indices]:
-                            self.all_models.append(get_model(model_name, device=x.device).model)
-                    rand_idx = np.random.choice(len(self.all_models))
-                    self.model = self.all_models[rand_idx]
-                    self.model = self.model.train()
-                if False and self.source and model_perturb: 
-                    raise NotImplementedError("Noise injection model perturbation is still under development")
-                    print('injecting noise injection')
+                if self.source: 
+                    raise NotImplementedError("Source functions are still under development")
                     x_with_units = x * self.scale + self.center
-                    old_with_units = old * self.scale + self.center
                     dt = torch.tensor(self.time_step.total_seconds())
-                    #x += self.source(x_with_units, time) / self.scale * dt
-
-                    #Multiplicative perturbation
-                    #x = x_with_units * self.source(x_with_units, time)
-
-                    #Multiplicative Tendency perturbation
-                    tendency = x_with_units - old_with_units
-                    tendency = tendency * self.source(x_with_units, time)
-                    x = old_with_units + tendency
-                    x = (x - self.center) / self.scale
-                old = x.clone().detach()
+                    x += self.source(x_with_units, time) / self.scale * dt
                 x = self.model(x, time)
                 time = time + self.time_step
 
